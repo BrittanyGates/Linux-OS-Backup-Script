f08d02f0e2efd7df5dd21c6b27db2f52e4369285
Brittany Gates <bcgates82@gmail.com>
2024-12-26 14:01:13 -0600

    Uploaded files.
    Uploaded the files for the branch.

---
+++ linux_os_backup_script.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Linux OS Backup Script
+Creator: Brittany Gates (https://github.com/brittbot-bgates) | (https://www.linkedin.com/in/brittanycgates) |
+(https://brittbot.com/)
+About: Back up specific directories in the Home Directory to a local backup device.
+"""
+
+import os
+import datetime
+
+today_date = datetime.date.today()
+today_date = str(today_date)
+
+
+def main() -> None:
+    """Runs the rsync command to back up specific directories onto a backup device.
+    :return: None
+    """
+    os.system(
+        "rsync -av --exclude={'.*', 'add_additional_folders_here'} /home/$username/ /mnt/backup_disk_path/-" + today_date)
+
+
+if __name__ == '__main__':
+    main()
